5a4bc0cd955cd692d231973588441595faa1156d
ahbejarano <ahbejarano@gmail.com>
2026-02-08 19:18:50 +0100

    Add GUI for lights

--- booksamples/chapter-15/src/main.zig
+++ booksamples/chapter-15/src/main.zig
@@ -3,6 +3,7 @@ const sdl3 = @import("sdl3");
 const std = @import("std");
 const zm = @import("zm");
 const log = std.log.scoped(.main);
+const zgui = @import("zgui");
 const zstbi = @import("zstbi");
 
 pub fn main() !void {
@@ -19,13 +20,13 @@ pub fn main() !void {
 const Game = struct {
     const ENTITY_ID: []const u8 = "SponzaEntity";
 
+    lightAngle: f32 = 140.0,
+
     pub fn cleanup(self: *Game) void {
         _ = self;
     }
 
     pub fn init(self: *Game, engCtx: *eng.engine.EngCtx, arenaAlloc: std.mem.Allocator) !eng.engine.InitData {
-        _ = self;
-
         const sponzaModel = try eng.mdata.loadModel(arenaAlloc, "res/models/sponza/Sponza.json");
         const models = try arenaAlloc.alloc(eng.mdata.ModelData, 1);
         models[0] = sponzaModel;
@@ -50,23 +51,120 @@ const Game = struct {
             .color = zm.Vec{ 1.0, 1.0, 1.0, 1.0 },
             .directional = true,
             .intensity = 1.0,
-            .pos = zm.Vec{ 0.0, -1.0, 0.0, 0.0 },
+            .pos = zm.Vec{
+                0.0,
+                -std.math.sin(std.math.degreesToRadians(self.lightAngle)),
+                -std.math.cos(std.math.degreesToRadians(self.lightAngle)),
+                0.0,
+            },
         };
         try engCtx.scene.addLight(engCtx.allocator, dirLight);
 
-        const pointLight = eng.scn.Light{
+        const pointLight1 = eng.scn.Light{
             .color = zm.Vec{ 0.0, 1.0, 0.0, 1.0 },
             .directional = false,
             .intensity = 1.0,
-            .pos = zm.Vec{ 5.0, 3.4, 1.2, 0.0 },
+            .pos = zm.Vec{ 5.0, 4.5, -2.5, 0.0 },
         };
-        try engCtx.scene.addLight(engCtx.allocator, pointLight);
+        try engCtx.scene.addLight(engCtx.allocator, pointLight1);
+        const pointLight2 = eng.scn.Light{
+            .color = zm.Vec{ 1.0, 0.0, 0.0, 1.0 },
+            .directional = false,
+            .intensity = 1.0,
+            .pos = zm.Vec{ 5.0, 4.5, -6.2, 0.0 },
+        };
+        try engCtx.scene.addLight(engCtx.allocator, pointLight2);
 
         return .{ .models = models, .materials = materials };
     }
 
+    fn handleGui(self: *Game, engCtx: *eng.engine.EngCtx) !bool {
+        const scene = &engCtx.scene;
+        const mouseState = engCtx.wnd.mouseState;
+        zgui.io.addMousePositionEvent(mouseState.x, mouseState.y);
+        zgui.io.addMouseButtonEvent(zgui.MouseButton.left, mouseState.flags.left);
+        zgui.io.addMouseButtonEvent(zgui.MouseButton.middle, mouseState.flags.middle);
+        zgui.io.addMouseButtonEvent(zgui.MouseButton.right, mouseState.flags.right);
+
+        var open = true;
+        zgui.newFrame();
+        zgui.setNextWindowPos(.{ .x = 0, .y = 0 });
+        zgui.setNextWindowSize(.{ .w = 300, .h = 400 });
+        _ = zgui.begin("Lights", .{ .popen = &open });
+        zgui.separatorText("Ambient Light");
+        var ambientCol = [3]f32{ scene.ambientLight[0], scene.ambientLight[1], scene.ambientLight[2] };
+        if (zgui.colorEdit3("Color", .{ .col = &ambientCol, .flags = zgui.ColorEditFlags{} })) {
+            scene.ambientLight[0] = ambientCol[0];
+            scene.ambientLight[1] = ambientCol[1];
+            scene.ambientLight[2] = ambientCol[2];
+        }
+        var ambientLightIntensity = scene.ambientLight[3];
+        if (zgui.inputFloat("Intensity", .{ .v = &ambientLightIntensity, .step = 0.1 })) {
+            scene.ambientLight[3] = ambientLightIntensity;
+        }
+
+        const lights = &scene.lights;
+        const numLights = lights.items.len;
+        for (0..numLights) |i| {
+            var light = &lights.items[i];
+
+            var buf1: [32:0]u8 = undefined;
+            const lightId = try std.fmt.bufPrintZ(&buf1, "Light-{d}", .{i});
+            zgui.separatorText(lightId);
+
+            var buf2: [32:0]u8 = undefined;
+            const dirId = try std.fmt.bufPrintZ(&buf2, "Directional-{d}", .{i});
+            var dirLight: bool = light.directional;
+            _ = zgui.checkbox(dirId, .{ .v = &dirLight });
+            if (light.directional) {
+                var buf3: [32:0]u8 = undefined;
+                const posId = try std.fmt.bufPrintZ(&buf3, "Angle-{d}", .{i});
+                if (zgui.dragFloat(posId, .{ .v = &self.lightAngle, .speed = 1.00 })) {
+                    if (self.lightAngle < 0) {
+                        self.lightAngle = 0;
+                    } else if (self.lightAngle > 180) {
+                        self.lightAngle = 180;
+                    }
+                    light.pos[1] = -std.math.sin(std.math.degreesToRadians(self.lightAngle));
+                    light.pos[2] = -std.math.cos(std.math.degreesToRadians(self.lightAngle));
+                    light.pos = zm.normalize4(light.pos);
+                }
+            } else {
+                var lightPos = [3]f32{ light.pos[0], light.pos[1], light.pos[2] };
+                var buf3: [32:0]u8 = undefined;
+                const posId = try std.fmt.bufPrintZ(&buf3, "Position-{d}", .{i});
+                if (zgui.dragFloat3(posId, .{ .v = &lightPos, .speed = 0.05 })) {
+                    light.pos[0] = lightPos[0];
+                    light.pos[1] = lightPos[1];
+                    light.pos[2] = lightPos[2];
+                }
+            }
+
+            var lightCol = [3]f32{ light.color[0], light.color[1], light.color[2] };
+            var buf4: [32:0]u8 = undefined;
+            const lightColorId = try std.fmt.bufPrintZ(&buf4, "Color-{d}", .{i});
+
+            if (zgui.colorEdit3(lightColorId, .{ .col = &lightCol, .flags = zgui.ColorEditFlags{} })) {
+                light.color[0] = lightCol[0];
+                light.color[1] = lightCol[1];
+                light.color[2] = lightCol[2];
+            }
+        }
+        zgui.end();
+        zgui.endFrame();
+        zgui.render();
+
+        return zgui.io.getWantCaptureKeyboard() or zgui.io.getWantCaptureMouse();
+    }
+
     pub fn input(self: *Game, engCtx: *eng.engine.EngCtx, deltaSec: f32) void {
-        _ = self;
+        const guiHandled: bool = self.handleGui(engCtx) catch |err| {
+            std.log.err("Error in handleGui: {any}", .{err});
+            return;
+        };
+        if (guiHandled) {
+            return;
+        }
         const inc: f32 = 10;
         var viewData = &engCtx.scene.camera.viewData;
         if (engCtx.wnd.isKeyPressed(sdl3.Scancode.w)) {
